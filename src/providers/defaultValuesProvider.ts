be71edf030c0bcb50fd4b100a99fd2a42bc05495
JVmano <jdefreitasmelo@gmail.com>
2022-09-03 15:56:55 -0300

    feat: set up default values provider

---
+++ src/providers/defaultValuesProvider.ts
@@ -0,0 +1,12 @@
+export const cityCodesProvider = [
+  { id: '011', slug: '011 - São Paulo' },
+  { id: '016', slug: '016 - São Paulo' },
+  { id: '017', slug: '017 - São Paulo' },
+  { id: '018', slug: '018 - São Paulo' }
+]
+
+export const callPlansProvider = [
+  { id: '30', slug: 'FaleMais 30' },
+  { id: '60', slug: 'FaleMais 60' },
+  { id: '120', slug: 'FaleMais 120' }
+]
