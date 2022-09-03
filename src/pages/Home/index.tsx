0748eb65c6291e7fc7ec6980aa61a4dc331906f9
JVmano <jdefreitasmelo@gmail.com>
2022-09-03 15:57:58 -0300

    feat: add state data values

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,74 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../Components/Button'
 import Input from '../../Components/Input'
+import { callPlansProvider, cityCodesProvider } from '../../providers/defaultValuesProvider'
 import { ButtonsGroup, Container, Select } from './style'
 
-export default function Home () {
+export default function Home (): JSX.Element {
+  const [originCity, setOriginCity] = useState('')
+  const [destinationCity, setDestinationCity] = useState('')
+  const [callTime, setCallTime] = useState('')
+  const [plan, setPlan] = useState('')
+
   return (
     <>
       <Container>
         <h2>Telzir</h2>
         <h1>Fale<span>Mais</span></h1>
         <Select
-          name='name'
+          name='originCity'
+          value={originCity}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) => setOriginCity(e.target.value)}
         >
           <option value={''} selected disabled hidden>
             Origin City
           </option>
-          <option value={'011'}>
-            011 - São Paulo
-          </option>
-          <option value={'016'}>
-            016 - São Paulo
-          </option>
-          <option value={'017'}>
-            017 - São Paulo
-          </option>
-          <option value={'018'}>
-            018 - São Paulo
-          </option>
+          {cityCodesProvider.map((city) => {
+            return (
+              <option key={city.id} value={city.id}>
+                {city.slug}
+              </option>
+            )
+          })}
         </Select>
         <Select
-          name='name'
+          name='destinationCity'
+          value={destinationCity}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) => setDestinationCity(e.target.value)}
         >
           <option value={''} selected disabled hidden>
             Destination City
           </option>
-          <option value={'011'}>
-            011 - São Paulo
-          </option>
-          <option value={'016'}>
-            016 - São Paulo
-          </option>
-          <option value={'017'}>
-            017 - São Paulo
-          </option>
-          <option value={'018'}>
-            018 - São Paulo
-          </option>
+          {cityCodesProvider.map((city) => {
+            return (
+              <option key={city.id} value={city.id}>
+                {city.slug}
+              </option>
+            )
+          })}
         </Select>
         <Input
-          name='name'
-          onChange={''}
-          value={''}
+          name='callTime'
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) => setCallTime(e.target.value)}
+          value={callTime}
           placeholder={'Call Time in Minutes'}
           type={'text'}
         />
         <Select
-          name='name'
+          name='plan'
+          value={plan}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) => setPlan(e.target.value)}
         >
           <option value={''} selected disabled hidden>
             FaleMais Plan
           </option>
-          <option value={'FaleMais 30'}>
-            FaleMais 30
-          </option>
-          <option value={'FaleMais 60'}>
-            FaleMais 60
-          </option>
-          <option value={'FaleMais 120'}>
-            FaleMais 120
-          </option>
+          {callPlansProvider.map((plan) => {
+            return (
+              <option key={plan.id} value={plan.id}>
+                {plan.slug}
+              </option>
+            )
+          })}
         </Select>
 
         <ButtonsGroup>
