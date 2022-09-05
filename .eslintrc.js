d2b0532f2ed8f170baca64d21965649a198c2dfb
JVmano <jdefreitasmelo@gmail.com>
2022-09-05 17:48:27 -0300

    fix: update eslint conflict with react build

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,25 @@ module.exports = {
     es2021: true,
     jest: true
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 'latest',
-    prettier: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+  },
   globals: {
     JSX: 'readonly'
-  },
-  files: [
-    '**/*.test.ts',
-    '**/*.test.tsx'
-  ]
+  }
 }
